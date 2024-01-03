aaf2e2f9aec71459f12d147caea0342ec8c6339f
Brian Cline <brian.cline@gmail.com>
2024-01-02 21:14:43 -0600

    Add rough initial pass of playlist data archiver

---
+++ playlist_archive.py
@@ -0,0 +1,70 @@
+# import argparse
+import sys
+
+import rich.console
+import rich.theme
+
+import ytutil
+
+console = rich.console.Console(theme=rich.theme.Theme({'repr.number': 'bold'}))
+
+
+def print_info(msg):
+    console.print(msg, style='white', markup=False)
+
+
+def print_detail(msg):
+    console.print(msg, style='dim', markup=False)
+
+
+def truncate_title(title, max_len=70):
+    safe_len = max_len - 3
+    return f'{title[0:safe_len]}...' if len(title) >= max_len + 3 else title
+
+
+def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('-p', '--playlist-id', required=False)
+    # args = parser.parse_args()
+    youtube = ytutil.youtube_init('creds-user.json')
+
+    print_info('Retrieving playlists')
+    playlists = ytutil.get_playlists(youtube)
+    playlists = sorted(playlists, key=lambda x: x['snippet']['title'].strip().lower())
+
+    print_info(f'Archiving list of {len(playlists)} playlists')
+    ytutil.save_json_file(playlists, 'data/playlists.json')
+
+    all_videos = {}
+    all_video_ids = []
+    for pl_num, pl in enumerate(playlists):
+        pl_id = pl['id']
+        pl_name = pl['snippet']['title']
+
+        print_info(f'[{pl_num + 1}/{len(playlists)}] {pl_name}')
+        pl_items = ytutil.get_playlist_items(pl_id, youtube)
+
+        print_detail(f'  - Archiving metadata for {len(pl_items)} playlist items')
+        ytutil.save_json_file(pl_items, f'data/playlist-items-{pl_id}.json')
+
+        for vid_id in [x['contentDetails']['videoId'] for x in pl_items]:
+            all_video_ids.append(vid_id)
+
+    all_video_ids = list(set(all_video_ids))
+    print_info(f'Retrieving video metadata for {len(all_video_ids)} videos')
+    all_videos = ytutil.get_videos(all_video_ids, youtube)
+
+    print_info(f'Archiving video metadata for {len(all_videos)} videos)')
+    for vid_id, vid in all_videos.items():
+        vid_title = truncate_title(vid['snippet']['title'], 60)
+        print_detail(f'  - [{vid_id}] {vid_title}')
+        ytutil.save_json_file(vid, f'data/videos/video-{vid_id}.json')
+
+    unavail_count = len(all_video_ids) - len(all_videos)
+    print_info(f'Archived metadata for {len(playlists)} playlists, {len(all_videos)} videos. '
+               f'Skipped {unavail_count} unavailable videos.')
+    return True
+
+
+if __name__ == "__main__":
+    sys.exit(0 if main() else 1)
