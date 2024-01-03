91010a9cb1f254ce3aebb8c2d79a6f6d6b8e1859
Brian Cline <brian.cline@gmail.com>
2024-01-02 19:50:57 -0600

    Consolidate YT API pagination code in one spot

--- ytutil/__init__.py
+++ ytutil/__init__.py
@@ -13,6 +13,10 @@ API_SERVICE_NAME = "youtube"
 API_VERSION = "v3"
 # USER_DATA_SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
 
+MAX_PAGE_SIZE = 50
+
+# TODO: add logger to curtail my print() proclivities
+
 
 def youtube_init(creds_file: pathlib.Path):
     with open(creds_file, 'r', encoding='utf-8') as fd:
@@ -45,77 +49,68 @@ def video_duration_str(iso_duration: str) -> str:
     return format_duration(iso_duration_to_seconds(iso_duration))
 
 
-def get_playlists(youtube: googleapiclient.discovery.Resource):
-    playlists_svc = youtube.playlists()
-    results = []
-    request = playlists_svc.list(part="snippet,contentDetails", maxResults=50, mine=True)
-    page_num = 0
-    while request is not None:
-        page_num += 1
-        # print(f'Requesting playlists page {page_num}...')
-
-        response = request.execute()
-        for item in response['items']:
-            results.append(item)
-
-        request = playlists_svc.list_next(request, response)
-
-    return results
-
-
 def iso_duration_to_seconds(iso_duration: str) -> float:
     delta = isodate.parse_duration(iso_duration)
     return delta.total_seconds()
 
 
-def get_playlist_items(playlist_id: str, youtube: googleapiclient.discovery.Resource):
-    plitems_svc = youtube.playlistItems()
-    videos = []
-    request = plitems_svc.list(playlistId=playlist_id, part="snippet,contentDetails", maxResults=50)
+def list_all(svc, properties=None, **kwargs):
+    properties = properties or 'snippet'
+    if 'maxResults' not in kwargs:
+        kwargs['maxResults'] = MAX_PAGE_SIZE
+
+    request = svc.list(part=properties, **kwargs)
+
     page_num = 0
+    records = []
     while request is not None:
         page_num += 1
-        # print(f'Requesting playlistitems page {page_num}...')
+        # print(f'Requesting page {page_num}...')
 
         response = request.execute()
         for item in response['items']:
-            videos.append(item)
+            records.append(item)
 
-        request = plitems_svc.list_next(request, response)
+        request = svc.list_next(request, response)
 
-    return videos
+    return records
+
+
+def get_playlists(youtube: googleapiclient.discovery.Resource):
+    return list_all(
+        youtube.playlists(),
+        properties='snippet,contentDetails',
+        mine=True,
+    )
+
+
+def get_playlist_items(playlist_id: str, youtube: googleapiclient.discovery.Resource):
+    return list_all(
+        youtube.playlistItems(),
+        playlistId=playlist_id,
+        properties='snippet,contentDetails',
+    )
 
 
 def get_videos(video_ids: list[str], youtube: googleapiclient.discovery.Resource):
+    properties = 'snippet,contentDetails,statistics'
     vid_svc = youtube.videos()
     videos = {}
-    chunk_count = 50
+    chunk_count = MAX_PAGE_SIZE
     chunk_num = 0
+
     for chunk_start in range(0, len(video_ids), chunk_count):
         chunk_num += 1
         chunk_end = chunk_start + chunk_count
         chunk_ids = video_ids[chunk_start:chunk_end]
         vid_ids_str = ','.join(chunk_ids)
+
         # print(f'Chunking IDs [{chunk_start}, {chunk_end}]')
         # print(f'  {chunk_ids}')
         # print(f'  {vid_ids_str}')
 
-        request: googleapiclient.http.HttpRequest = vid_svc.list(
-            part="snippet,contentDetails,statistics",
-            id=vid_ids_str,
-            maxResults=chunk_count,
-        )
-
-        page_num = 0
-        while request is not None:
-            page_num += 1
-            # print(f'Requesting bulk videos chunk {chunk_num} page {page_num}...')
-
-            response = request.execute()
-            for item in response['items']:
-                vid_id = item['id']
-                videos[vid_id] = item
-
-            request = vid_svc.list_next(request, response)
+        for item in list_all(vid_svc, id=vid_ids_str, properties=properties,
+                             maxResults=chunk_count):
+            videos[item['id']] = item
 
     return videos
